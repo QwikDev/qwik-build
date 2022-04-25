beb3452efeb19f37266ae0dd21b82b7c866ce459
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-04-25 21:13:17 +0000

    66e4165 Merge branch 'main' into tutorials
    https://github.com/BuilderIO/qwik/commit/66e416575a749871d36fd0bac4591b0d9718add3

--- build/index.mjs
+++ build/index.mjs
@@ -1,6 +1,6 @@
 // src/build/index.ts
-var isServer = /* @__PURE__ */ (() => typeof process !== "undefined" && process.versions != null && process.versions.node != null)();
-var isBrowser = /* @__PURE__ */ (() => typeof window !== "undefined" && window.document != null)();
+var isServer = /* @__PURE__ */ (() => typeof process !== "undefined" && !!process.versions && !!process.versions.node || typeof Deno !== "undefined")();
+var isBrowser = /* @__PURE__ */ (() => typeof window !== "undefined" && !!window.document || typeof self !== "undefined" && typeof self.importScripts === "function")();
 export {
   isBrowser,
   isServer
