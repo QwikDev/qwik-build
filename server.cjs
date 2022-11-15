90c0a2924134c815f6a5051c3af9e4d0f3f25bfe
Adam Bradley <adamdbradley@users.noreply.github.com>
2022-11-15 00:44:53 +0000

    0a0260b chore: update manifest warning msg (#2139)
    https://github.com/BuilderIO/qwik/commit/0a0260bdf73203169e232d7eb8ff2f3d8d9a386b

--- server.cjs
+++ server.cjs
@@ -647,7 +647,9 @@ async function renderToStream(rootNode, opts) {
     }
   }
   if (!opts.manifest) {
-    console.warn("Missing client manifest, loading symbols in the client might 404");
+    console.warn(
+      `Missing client manifest, loading symbols in the client might 404. Please ensure the client build has run and generated the manifest for the server build.`
+    );
   }
   const buildBase = getBuildBase(opts);
   const resolvedManifest = resolveManifest(opts.manifest);
