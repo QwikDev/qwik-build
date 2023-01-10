b08e95d0950476ae7e4e4d8549dc5a2b3bc4db42
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-01-10 16:50:20 +0000

    bd563fb fix: adaptor types
    https://github.com/BuilderIO/qwik/commit/bd563fb5dbd304b433c456dfcd4b833f85792f8e

--- core.d.ts
+++ core.d.ts
@@ -3601,7 +3601,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230110101017
+ * 0.16.2-dev20230110164845
  * @public
  */
 export declare const version: string;
