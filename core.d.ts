1dd1f12196b6957639101f637d185bc62e5524f1
Manu MA <manu.mtza@gmail.com>
2023-01-11 10:42:02 +0000

    b0af4ee fix: string wrapSignal() (#2612)
    https://github.com/BuilderIO/qwik/commit/b0af4ee7d852c48ab07bb2f9ef1682a799289802

--- core.d.ts
+++ core.d.ts
@@ -3601,7 +3601,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230110224242
+ * 0.16.2-dev20230111104001
  * @public
  */
 export declare const version: string;
