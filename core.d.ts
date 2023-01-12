89ccf4b53c54b6888e5ea24e053e4d70fb1af590
Manu MA <manu.mtza@gmail.com>
2023-01-12 09:28:50 +0000

    1e193fc fix: external redirect in actions
    https://github.com/BuilderIO/qwik/commit/1e193fc56b59f73392c72c2fa172ef1e6c0baf16

--- core.d.ts
+++ core.d.ts
@@ -3619,7 +3619,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230111160518
+ * 0.16.2-dev20230112092645
  * @public
  */
 export declare const version: string;
