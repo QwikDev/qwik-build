12c37f19c56012d36c663051729b23616e4fc5d6
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-01-12 16:18:20 +0000

    eac7766 🏈
    https://github.com/BuilderIO/qwik/commit/eac7766ab065bedeb3d1a58f760028009ddb3dac

--- core.d.ts
+++ core.d.ts
@@ -3619,7 +3619,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230112160755
+ * 0.16.2-dev20230112161641
  * @public
  */
 export declare const version: string;
