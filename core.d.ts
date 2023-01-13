948f3ca6f11e5022fad5f48ad4fdfd645bbc786c
Manu MA <manu.mtza@gmail.com>
2023-01-13 20:39:39 +0000

    ba09db9 feat: add env to request event (#2635)
    https://github.com/BuilderIO/qwik/commit/ba09db947c8772643d7237fae13deba6f7fb1703

--- core.d.ts
+++ core.d.ts
@@ -3619,7 +3619,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230112181122
+ * 0.16.2-dev20230113203806
  * @public
  */
 export declare const version: string;
