d1014e5bfa59c77957c28f816b904894cc5e76a2
Manu MA <manu.mtza@gmail.com>
2023-01-13 18:02:05 +0000

    b4afb67 📍
    https://github.com/BuilderIO/qwik/commit/b4afb67a1ca36b8af27a632e573f7e1e75ec0752

--- core.d.ts
+++ core.d.ts
@@ -3619,7 +3619,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230113173141
+ * 0.16.2-dev20230113175947
  * @public
  */
 export declare const version: string;
