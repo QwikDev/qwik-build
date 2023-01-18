0f0540f37f0ca06bc545714ccb291569e8e88aee
Manu MA <manu.mtza@gmail.com>
2023-01-18 11:03:42 +0000

    aad7742 fix: action isRunning (#2661)
    https://github.com/BuilderIO/qwik/commit/aad7742d631c6e758d56c1ee94cd28881a00c73b

--- core.d.ts
+++ core.d.ts
@@ -3619,7 +3619,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230117092004
+ * 0.16.2-dev20230118110151
  * @public
  */
 export declare const version: string;
