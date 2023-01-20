1370cc07776b28f73865eefa4ceb86dc0e75a7a8
Miško Hevery <misko@hevery.com>
2023-01-20 22:27:58 +0000

    8761396 chore: improve build performance by doing shallow git clone on build repos (#2687)
    https://github.com/BuilderIO/qwik/commit/87613961762df5557cd06bb1a5ae9dcb1c9286e0

--- core.d.ts
+++ core.d.ts
@@ -3619,7 +3619,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230120203602
+ * 0.16.2-dev20230120222657
  * @public
  */
 export declare const version: string;
