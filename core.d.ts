e6fd253c24e31e240533c99ec03512a065ccf9b9
Manu MA <manu.mtza@gmail.com>
2023-01-27 14:53:17 +0000

    6bdd9aa fix: cloudflare use worker conditionals (#2731)
    https://github.com/BuilderIO/qwik/commit/6bdd9aa126b67706ec0313424e32f6bebbd1917a

--- core.d.ts
+++ core.d.ts
@@ -3619,7 +3619,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230127001022
+ * 0.16.2-dev20230127145221
  * @public
  */
 export declare const version: string;
