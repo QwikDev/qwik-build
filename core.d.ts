cee76d7b7cfa9223bf05401ae67677323ef5af01
Manu MA <manu.mtza@gmail.com>
2023-01-27 14:03:51 +0000

    9a47f28 Merge branch 'main' into server-fn-docs
    https://github.com/BuilderIO/qwik/commit/9a47f28382947cb0e1832b775f225e2240a64a08

--- core.d.ts
+++ core.d.ts
@@ -3619,7 +3619,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230127114241
+ * 0.16.2-dev20230127140257
  * @public
  */
 export declare const version: string;
