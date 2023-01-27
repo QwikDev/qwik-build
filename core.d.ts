59cd2382dab24ff95d2ee38e2047e28514888339
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-01-27 17:11:04 +0000

    209985e fix: serve static q-data.json (#2735)
    https://github.com/BuilderIO/qwik/commit/209985eb2b4160e25a141c01a49251138b1503ef

--- core.d.ts
+++ core.d.ts
@@ -3619,7 +3619,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230127145221
+ * 0.16.2-dev20230127171010
  * @public
  */
 export declare const version: string;
