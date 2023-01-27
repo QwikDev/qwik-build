02f1386fcf5504551bc4f1a8051892694ac3620b
Manu MA <manu.mtza@gmail.com>
2023-01-27 21:37:16 +0000

    ac69fe8 docs: about loader until finalized (#2740)
    https://github.com/BuilderIO/qwik/commit/ac69fe82746bcbe46a437915ca2b7b62f916ff00

--- core.d.ts
+++ core.d.ts
@@ -3619,7 +3619,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230127192933
+ * 0.16.2-dev20230127213621
  * @public
  */
 export declare const version: string;
