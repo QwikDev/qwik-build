595fcd550783b3ceb11c9d0f251a528b332c8d60
Manu MA <manu.mtza@gmail.com>
2023-01-28 09:11:47 +0000

    dd2e1ef docs: enable cache (#2744)
    https://github.com/BuilderIO/qwik/commit/dd2e1ef61b9e6731ac62ea05b422dfba2011eeb3

--- core.d.ts
+++ core.d.ts
@@ -3619,7 +3619,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230128090154
+ * 0.16.2-dev20230128091053
  * @public
  */
 export declare const version: string;
