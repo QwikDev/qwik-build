49f8e5eab05916099d20cbe4946f8946c17db983
Manu MA <manu.mtza@gmail.com>
2023-01-29 20:41:47 +0000

    591ad58 fix: action$() can be declared anywhere (#2755)
    https://github.com/BuilderIO/qwik/commit/591ad586fde046617f512462217031ffe7683472

--- core.d.ts
+++ core.d.ts
@@ -3634,7 +3634,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230128164546
+ * 0.16.2-dev20230129204054
  * @public
  */
 export declare const version: string;
