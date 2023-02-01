8ae332e8c4e29f6e4d2b13ad52da3868d70fb2fe
Manu MA <manu.mtza@gmail.com>
2023-02-01 20:28:17 +0000

    774670c Merge branch 'main' into fix-spa-actions
    https://github.com/BuilderIO/qwik/commit/774670cee6e999c97a035ffe963680e8a176ba9e

--- core.d.ts
+++ core.d.ts
@@ -3634,7 +3634,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230201124133
+ * 0.16.2-dev20230201202722
  * @public
  */
 export declare const version: string;
