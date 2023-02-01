8e769f7efaf8f5b5c15da206fa6d43a73afc6cb1
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-02-01 13:55:02 +0000

    7826ffa fix: log server runtime error (#2773)
    https://github.com/BuilderIO/qwik/commit/7826ffa1028ce8dd2d8a23221920bab3b4a4a3bc

--- core.d.ts
+++ core.d.ts
@@ -3634,7 +3634,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.16.2-dev20230131143306
+ * 0.16.2-dev20230201135402
  * @public
  */
 export declare const version: string;
