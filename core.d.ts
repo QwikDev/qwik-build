f8444b60daf7706caeac24adc7b5d4e382d13f51
Manu MA <manu.mtza@gmail.com>
2023-02-02 16:41:05 +0000

    65e3b16 🐚
    https://github.com/BuilderIO/qwik/commit/65e3b164286150abf72ccd69297a44ee8b191fc5

--- core.d.ts
+++ core.d.ts
@@ -3626,7 +3626,7 @@ export declare const useWatchQrl: (qrl: QRL<TaskFn>, opts?: UseTaskOptions) => v
 export declare type ValueOrPromise<T> = T | Promise<T>;
 
 /**
- * 0.17.0-dev20230202163418
+ * 0.17.1-dev20230202164002
  * @public
  */
 export declare const version: string;
--- core.prod.mjs
+++ core.prod.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 0.17.0
+ * @builder.io/qwik 0.17.1
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
@@ -3752,7 +3752,7 @@ const Slot = props => {
     }, name);
 };
 
-const version = "0.17.0";
+const version = "0.17.1";
 
 const render = async (parent, jsxNode, opts) => {
     isJSXNode(jsxNode) || (jsxNode = jsx(jsxNode, null));
@@ -3784,7 +3784,7 @@ const renderRoot$1 = async (parent, jsxNode, doc, containerState, containerEl) =
 const getElement = docOrElm => isDocument(docOrElm) ? docOrElm.documentElement : docOrElm;
 
 const injectQContainer = containerEl => {
-    directSetAttribute(containerEl, "q:version", "0.17.0"), directSetAttribute(containerEl, "q:container", "resumed"), 
+    directSetAttribute(containerEl, "q:version", "0.17.1"), directSetAttribute(containerEl, "q:container", "resumed"), 
     directSetAttribute(containerEl, "q:render", "dom");
 };
 
@@ -4076,7 +4076,7 @@ const _renderSSR = async (node, opts) => {
     const containerAttributes = {
         ...opts.containerAttributes,
         "q:container": "paused",
-        "q:version": "0.17.0",
+        "q:version": "0.17.1",
         "q:render": qRender,
         "q:base": opts.base,
         "q:locale": opts.serverData?.locale,
