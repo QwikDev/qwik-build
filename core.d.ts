b2197a96efd6de46af1c7a80f4885d2672638227
Manu MA <manu.mtza@gmail.com>
2023-02-03 17:35:00 +0000

    f2390a6 fix: clean cache on failed SPA nav (#2811)
    https://github.com/BuilderIO/qwik/commit/f2390a6b26fc990a18f5d410957f9ced4e040d08

--- core.d.ts
+++ core.d.ts
@@ -3631,7 +3631,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.17.2-dev20230203171409
+ * 0.17.2-dev20230203173405
  * @public
  */
 export declare const version: string;
