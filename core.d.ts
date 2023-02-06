300820d036a28c749ef5320f7bee211cbaa6bbd3
Manu MA <manu.mtza@gmail.com>
2023-02-06 18:18:12 +0000

    2bb0a7f fix: misc loader fixes (#2842)
    https://github.com/BuilderIO/qwik/commit/2bb0a7fa53c8887a1cea4fb21311c6445b3e3807

--- core.d.ts
+++ core.d.ts
@@ -3631,7 +3631,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.17.4-dev20230206171259
+ * 0.17.4-dev20230206181716
  * @public
  */
 export declare const version: string;
