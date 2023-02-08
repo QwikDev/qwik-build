81aeb5948637d80343b8e438b1ceea69c44ae63a
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-02-08 00:09:56 +0000

    60a2556 docs: add vercel edge functions to starters (#2870)
    https://github.com/BuilderIO/qwik/commit/60a2556b7bcaa8481737202d2eecb9e22ac38040

--- core.d.ts
+++ core.d.ts
@@ -3631,7 +3631,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.17.5-dev20230207223338
+ * 0.17.5-dev20230208000901
  * @public
  */
 export declare const version: string;
