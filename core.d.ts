6b4fed7ba017f27d1d9be3148a9359390d48d0ad
Manu MA <manu.mtza@gmail.com>
2023-02-10 18:08:18 +0000

    63e1f53 refactor: rename streaming to defer (#2925)
    https://github.com/BuilderIO/qwik/commit/63e1f535c7e3487339c5303ca6ed216b9ca2481f

--- core.d.ts
+++ core.d.ts
@@ -3631,7 +3631,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.17.6-dev20230210165449
+ * 0.17.6-dev20230210180724
  * @public
  */
 export declare const version: string;
