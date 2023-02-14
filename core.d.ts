0dbaf39fad28cd6e596ef2644706ef0e65828313
Manu MA <manu.mtza@gmail.com>
2023-02-14 14:21:24 +0000

    f75eae0 refactor: name getData to resolveValue (#2964)
    https://github.com/BuilderIO/qwik/commit/f75eae0b77530a93a9c3ff3ba0ed42f0cfca73f4

--- core.d.ts
+++ core.d.ts
@@ -3664,7 +3664,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.17.6-dev20230214135741
+ * 0.17.6-dev20230214142029
  * @public
  */
 export declare const version: string;
