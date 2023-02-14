a6996db85fb2c713cda198bdc9002e0c1fcc1af1
云斌 <caoyunbin001@126.com>
2023-02-14 13:58:38 +0000

    1fac572 fix: decodeURIComponent for url params (#2949)
    https://github.com/BuilderIO/qwik/commit/1fac572bce6deccb2912fec82ad81c97573e3526

--- core.d.ts
+++ core.d.ts
@@ -3664,7 +3664,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.17.6-dev20230213233206
+ * 0.17.6-dev20230214135741
  * @public
  */
 export declare const version: string;
