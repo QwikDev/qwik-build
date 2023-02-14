0484a1340523f0b93f75b4f45df8ea98a3bc9c5f
Manu MA <manu.mtza@gmail.com>
2023-02-14 17:40:16 +0000

    e206db7 fix: node build (#2974)
    https://github.com/BuilderIO/qwik/commit/e206db7d06aec1975b099a4f5b0473545b94565f

--- core.d.ts
+++ core.d.ts
@@ -3664,7 +3664,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.18.0-dev20230214154715
+ * 0.18.0-dev20230214173920
  * @public
  */
 export declare const version: string;
