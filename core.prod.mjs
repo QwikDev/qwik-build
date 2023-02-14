f6237d7b90c72b377cace09765b867c168169de8
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-02-14 18:53:50 +0000

    edf1453 docs: update to latest qwik (#2977)
    https://github.com/BuilderIO/qwik/commit/edf1453d3b1705cb195faed71a5623e0d69d9949

--- core.prod.mjs
+++ core.prod.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 0.18.0
+ * @builder.io/qwik 0.18.1
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
@@ -4177,7 +4177,7 @@ const Slot = props => {
     }, name);
 };
 
-const version = "0.18.0";
+const version = "0.18.1";
 
 const render = async (parent, jsxNode, opts) => {
     isJSXNode(jsxNode) || (jsxNode = jsx(jsxNode, null));
@@ -4212,7 +4212,7 @@ const renderRoot$1 = async (parent, jsxNode, doc, containerState, containerEl) =
 const getElement = docOrElm => isDocument(docOrElm) ? docOrElm.documentElement : docOrElm;
 
 const injectQContainer = containerEl => {
-    directSetAttribute(containerEl, "q:version", "0.18.0"), directSetAttribute(containerEl, "q:container", "resumed"), 
+    directSetAttribute(containerEl, "q:version", "0.18.1"), directSetAttribute(containerEl, "q:container", "resumed"), 
     directSetAttribute(containerEl, "q:render", qDev ? "dom-dev" : "dom");
 };
 
@@ -4514,7 +4514,7 @@ const _renderSSR = async (node, opts) => {
     const containerAttributes = {
         ...opts.containerAttributes,
         "q:container": "paused",
-        "q:version": "0.18.0",
+        "q:version": "0.18.1",
         "q:render": qRender,
         "q:base": opts.base,
         "q:locale": opts.serverData?.locale,
