bb5c4b8cf7719cc15b40cf6daa651583908a45ee
Manu MA <manu.mtza@gmail.com>
2023-02-22 16:07:00 +0000

    7908cce fix: auto generation of exports (#3084)
    https://github.com/BuilderIO/qwik/commit/7908ccec8993eec762e2118fcebd4c72c194affd

--- core.d.ts
+++ core.d.ts
@@ -3685,7 +3685,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.19.0-dev20230222151729
+ * 0.19.0-dev20230222160605
  * @public
  */
 export declare const version: string;
