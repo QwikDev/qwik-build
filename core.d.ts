e5b8686297acd80c8ba96389d3f3f337eaa2ecf6
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-02-24 00:34:52 +0000

    f00017a fix: dev server base path (#3127)
    https://github.com/BuilderIO/qwik/commit/f00017ae38db5b7ac902c498610b35d5a2b0e1b9

--- core.d.ts
+++ core.d.ts
@@ -3685,7 +3685,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.19.2-dev20230223172506
+ * 0.19.2-dev20230224003357
  * @public
  */
 export declare const version: string;
