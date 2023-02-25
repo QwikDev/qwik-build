58594e081aa0b849ac509e887c81ceabdde81594
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-02-25 03:44:16 +0000

    077585f fix: only pages in sitemap.xml (#3145)
    https://github.com/BuilderIO/qwik/commit/077585fa73cdaab853308ab968301b603faad022

--- core.d.ts
+++ core.d.ts
@@ -3685,7 +3685,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.19.2-dev20230224215954
+ * 0.19.2-dev20230225034323
  * @public
  */
 export declare const version: string;
