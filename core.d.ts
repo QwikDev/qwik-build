34619f46caa42daa81f62c5f60b6c96ce588b5dc
Manu MA <manu.mtza@gmail.com>
2023-02-25 19:34:14 +0000

    8888d73 fix: set locale regression (#3156)
    https://github.com/BuilderIO/qwik/commit/8888d73a38102f316ca84bfecd74a83920c8442f

--- core.d.ts
+++ core.d.ts
@@ -3685,7 +3685,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.19.2-dev20230225191025
+ * 0.19.2-dev20230225193314
  * @public
  */
 export declare const version: string;
