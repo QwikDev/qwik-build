97c564048568c35d79fc09c406aaa7a760917308
Manu MA <manu.mtza@gmail.com>
2023-02-27 12:17:21 +0000

    b874133 fix: action types (#3173)
    https://github.com/BuilderIO/qwik/commit/b8741338e0a881e46eb9b93b19ecf05af91eed71

--- core.d.ts
+++ core.d.ts
@@ -3685,7 +3685,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.19.3-dev20230227121329
+ * 0.19.3-dev20230227121626
  * @public
  */
 export declare const version: string;
