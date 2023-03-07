39256fe52c78925eec821448d732181650ecd31c
cunzaizhuyi <877824709@qq.com>
2023-03-07 08:45:26 +0000

    5d73a46 fix(starter): replace deprecated api to new api (#3229)
    https://github.com/BuilderIO/qwik/commit/5d73a46d112d1dc7399000a68c430b2924b818f9

--- core.d.ts
+++ core.d.ts
@@ -3733,7 +3733,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.20.2-dev20230307084241
+ * 0.20.2-dev20230307084431
  * @public
  */
 export declare const version: string;
