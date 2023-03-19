b5fd9de8b638db8caedb9f56b71509a182ce8034
Manu MA <manu.mtza@gmail.com>
2023-03-19 07:58:02 +0000

    4ff128f chore: improve docs (#3431)
    https://github.com/BuilderIO/qwik/commit/4ff128ff114a0037ae6227a9fcfc8f2b08fd8b35

--- core.d.ts
+++ core.d.ts
@@ -3780,7 +3780,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.23.0-dev20230318211642
+ * 0.23.0-dev20230319075706
  * @public
  */
 export declare const version: string;
