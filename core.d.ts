6118213b2855a82691bf78be9c555b24ec066b4a
Justin Scopelleti <38704268+Kesmek@users.noreply.github.com>
2023-03-21 07:02:56 +0000

    d68d347 fix(starter-basic): replace Deprecated loc.href With loc.url.href (#3452)
    https://github.com/BuilderIO/qwik/commit/d68d34723c35c39bb75b25fa03b147986d2be34c

--- core.d.ts
+++ core.d.ts
@@ -3784,7 +3784,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.23.0-dev20230320235303
+ * 0.23.0-dev20230321070205
  * @public
  */
 export declare const version: string;
