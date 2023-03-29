0f1f84c9a00981a24ebb1de9960ba5abfc8e9b80
Miško Hevery <misko@hevery.com>
2023-03-29 03:44:13 +0000

    19fa68f docs: cleanup
    https://github.com/BuilderIO/qwik/commit/19fa68f9b601eb0979df833ee2c5ece4e8e7b398

--- core.d.ts
+++ core.d.ts
@@ -3795,7 +3795,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.24.0-dev20230328171328
+ * 0.24.0-dev20230329034317
  * @public
  */
 export declare const version: string;
