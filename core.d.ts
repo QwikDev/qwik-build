aacff627bf44294a1b3f08ca947902bad6bbac1f
Manu MA <manu.mtza@gmail.com>
2023-04-02 20:53:02 +0000

    69ab5e9 docs: allow line and code highlighing (#3630)
    https://github.com/BuilderIO/qwik/commit/69ab5e9a57708ffd945e287feadfff3812aee6e0

--- core.d.ts
+++ core.d.ts
@@ -3558,7 +3558,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.100.0-dev20230402131238
+ * 0.100.0-dev20230402205155
  * @public
  */
 export declare const version: string;
