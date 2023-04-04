2f05ee93eecaaeb0527ed1ad86c5c7f193e1e253
Manu MA <manu.mtza@gmail.com>
2023-04-04 14:45:24 +0000

    829ba77 fix: render of spread children (#3657)
    https://github.com/BuilderIO/qwik/commit/829ba770e76b02ed4d3bb12744d0553bf78feee9

--- core.d.ts
+++ core.d.ts
@@ -3559,7 +3559,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.100.0-dev20230404104840
+ * 0.100.0-dev20230404144357
  * @public
  */
 export declare const version: string;
