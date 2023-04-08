c13798248c13b18c14cf1fe5942e080b164f4e59
Genki Takiuchi <genki@s21g.com>
2023-04-08 16:57:29 +0000

    96ce3b5 fix: load `.env` files on dev server (#2852)
    https://github.com/BuilderIO/qwik/commit/96ce3b59c37bbbaea9a73c9c9f3f181f11c8a01c

--- core.d.ts
+++ core.d.ts
@@ -3571,7 +3571,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.101.0-dev20230407214008
+ * 0.101.0-dev20230408165630
  * @public
  */
 export declare const version: string;
