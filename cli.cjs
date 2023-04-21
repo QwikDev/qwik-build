64f1f57626c756e87092444bd5095b653b4c4584
Manu MA <manu.mtza@gmail.com>
2023-04-21 12:41:14 +0000

    739d9ed fix: recursive tracking loaders (#3872)
    https://github.com/BuilderIO/qwik/commit/739d9edbaca2f4b445af8d233641abe099f66644

--- cli.cjs
+++ cli.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/cli 0.103.0-dev20230420183237
+ * @builder.io/qwik/cli 0.103.0-dev20230421124111
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
@@ -4099,7 +4099,7 @@ async function printHelp(app) {
   await runCommand2(Object.assign(app, { task: command }));
 }
 function printVersion() {
-  console.log("0.103.0-dev20230420183237");
+  console.log("0.103.0-dev20230421124111");
 }
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
--- core.d.ts
+++ core.d.ts
@@ -3570,7 +3570,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.103.0-dev20230420183140
+ * 0.103.0-dev20230421124017
  * @public
  */
 export declare const version: string;
