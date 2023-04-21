273b6e58b0d8dc356d951922e9f2b3872c645411
Sami Jaber <jabersami@gmail.com>
2023-04-21 21:07:41 +0000

    6b34cfe fix: Click-To-Source HTML (#3879)
    https://github.com/BuilderIO/qwik/commit/6b34cfe340574bbfe0fa53cd93c78ab2eb403cce

--- core.d.ts
+++ core.d.ts
@@ -3570,7 +3570,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.103.0-dev20230421124017
+ * 0.103.0-dev20230421210642
  * @public
  */
 export declare const version: string;
