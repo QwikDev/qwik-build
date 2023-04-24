7fa8dca56f62a746eca51ef297203c0f07fcebf4
Manu MA <manu.mtza@gmail.com>
2023-04-24 10:39:02 +0000

    38c4363 fix(eslint): allow use inside objects (#3902)
    https://github.com/BuilderIO/qwik/commit/38c436338c360e6b35590054a619fe6f4bb88104

--- cli.cjs
+++ cli.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/cli 0.103.0-dev20230424085026
+ * @builder.io/qwik/cli 0.103.0-dev20230424103858
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
@@ -4108,7 +4108,7 @@ async function printHelp(app) {
   await runCommand2(Object.assign(app, { task: command }));
 }
 function printVersion() {
-  console.log("0.103.0-dev20230424085026");
+  console.log("0.103.0-dev20230424103858");
 }
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
