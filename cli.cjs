119bcae41ff4305b9930685890f417ec6c300c17
Manu MA <manu.mtza@gmail.com>
2023-04-26 14:21:19 +0000

    fadc9e0 feat: expose whole cloudflare context (#3931)
    https://github.com/BuilderIO/qwik/commit/fadc9e01553f73698e35e96f15bade8e7910afe4

--- cli.cjs
+++ cli.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/cli 0.104.0-dev20230426090039
+ * @builder.io/qwik/cli 0.104.0-dev20230426142115
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
@@ -4124,7 +4124,7 @@ async function printHelp(app) {
   await runCommand2(Object.assign(app, { task: command }));
 }
 function printVersion() {
-  console.log("0.104.0-dev20230426090039");
+  console.log("0.104.0-dev20230426142115");
 }
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
--- core.d.ts
+++ core.d.ts
@@ -3570,7 +3570,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 0.104.0-dev20230426085928
+ * 0.104.0-dev20230426142015
  * @public
  */
 export declare const version: string;
