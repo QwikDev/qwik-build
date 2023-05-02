7cfa2de738b7d30895e3f2df9d16262a3f3c1db1
Manu MA <manu.mtza@gmail.com>
2023-05-02 22:57:05 +0000

    6897d9d feat: qwik-worker (#4014)
    https://github.com/BuilderIO/qwik/commit/6897d9d580eb7f591c6a44d766a1c4a779f5e4b0

--- core.prod.mjs
+++ core.prod.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 0.107.0
+ * @builder.io/qwik 1.0.0
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
@@ -2327,7 +2327,7 @@ const static_listeners = 1;
 
 const static_subtree = 2;
 
-const version = "0.107.0";
+const version = "1.0.0";
 
 var _a;
 
@@ -2388,7 +2388,7 @@ const _renderSSR = async (node, opts) => {
     const containerAttributes = {
         ...opts.containerAttributes,
         "q:container": "paused",
-        "q:version": "0.107.0",
+        "q:version": "1.0.0",
         "q:render": qRender,
         "q:base": opts.base,
         "q:locale": opts.serverData?.locale,
@@ -5165,7 +5165,7 @@ const renderRoot = async (rCtx, parent, jsxNode) => {
 const getElement = docOrElm => isDocument(docOrElm) ? docOrElm.documentElement : docOrElm;
 
 const injectQContainer = containerEl => {
-    directSetAttribute(containerEl, "q:version", "0.107.0"), directSetAttribute(containerEl, "q:container", "resumed"), 
+    directSetAttribute(containerEl, "q:version", "1.0.0"), directSetAttribute(containerEl, "q:container", "resumed"), 
     directSetAttribute(containerEl, "q:render", "dom");
 };
 
