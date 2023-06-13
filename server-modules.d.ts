1b97706dda0bd2967d70316b68f8c2c812343e1a
Manu MA <manu.mtza@gmail.com>
2023-06-13 19:47:35 +0000

    2f97a05 feat: image vite transform (#4479)
    https://github.com/BuilderIO/qwik/commit/2f97a05eaf55eeca4fff8e52731f756965c12915

--- server-modules.d.ts
+++ server-modules.d.ts
@@ -16,3 +16,11 @@ declare module '*.mdx' {
   export const frontmatter: Record<string, any>;
   export default node;
 }
+// Image ?jsx
+declare module '*?jsx' {
+  const Cmp: FunctionComponent<Omit<QwikIntrinsicElements['img'], 'src' | 'width' | 'height' | 'srcSet'>>
+  export default Cmp;
+  export const width: number;
+  export const height: number;
+  export const srcSet: string;
+}
