62d669b202658c91eeabe8340e17da2ff98fa34f
Manu MA <manu.mtza@gmail.com>
2023-06-27 14:31:11 +0000

    37b3a05 fix: image devtools (#4626)
    https://github.com/BuilderIO/qwik/commit/37b3a05b803a23c019c494be12d1a1fbea86c4b4

--- cli.cjs
+++ cli.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/cli 1.1.5
+ * @builder.io/qwik/cli 1.2.0
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
@@ -4539,7 +4539,7 @@ async function printHelp(app) {
   await runCommand2(Object.assign(app, { task: command }));
 }
 function printVersion() {
-  console.log("1.1.5");
+  console.log("1.2.0");
 }
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
--- core.d.ts
+++ core.d.ts
@@ -3701,7 +3701,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 1.1.5
+ * 1.2.0
  * @public
  */
 export declare const version: string;
