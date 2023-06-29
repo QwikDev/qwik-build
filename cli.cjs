7d29d8b27d66310e2a5fe17ed25d14d7eac9c55f
Manu MA <manu.mtza@gmail.com>
2023-06-29 19:42:29 +0000

    31140fb fix: csstypes dep (#4644)
    https://github.com/BuilderIO/qwik/commit/31140fb101ebd2d1568fb11bc7a79587924dafda

--- cli.cjs
+++ cli.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/cli 1.2.1
+ * @builder.io/qwik/cli 1.2.2
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
@@ -4538,7 +4538,7 @@ async function printHelp(app) {
   await runCommand2(Object.assign(app, { task: command }));
 }
 function printVersion() {
-  console.log("1.2.1");
+  console.log("1.2.2");
 }
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
--- optimizer.mjs
+++ optimizer.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/optimizer 1.2.1
+ * @builder.io/qwik/optimizer 1.2.2
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
@@ -1199,7 +1199,7 @@ var QWIK_BINDING_MAP = {
 };
 
 var versions = {
-  qwik: "1.2.1"
+  qwik: "1.2.2"
 };
 
 async function getSystem() {
