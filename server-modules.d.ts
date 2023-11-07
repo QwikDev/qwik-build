85d1483269458125c473b85f720eeecb2a422029
Miško Hevery <misko@hevery.com>
2023-11-07 11:11:42 +0000

    8d464db chore: add netlify debug
    https://github.com/BuilderIO/qwik/commit/8d464db7af0c8417ef72a5c1433f1bbf8c2627a9

---
+++ server-modules.d.ts
@@ -0,0 +1,34 @@
+/// <reference types="./server" />
+declare module '@qwik-client-manifest' {
+  const manifest: import('./optimizer').QwikManifest;
+  export { manifest };
+}
+// MD
+declare module '*.md' {
+  const node: import('./core').FunctionComponent;
+  export const frontmatter: Record<string, any>;
+  export default node;
+}
+// MDX
+declare module '*.mdx' {
+  const node: import('./core').FunctionComponent;
+  export const frontmatter: Record<string, any>;
+  export default node;
+}
+// Image ?jsx
+declare module '*?jsx' {
+  const Cmp: import('./core').FunctionComponent<Omit<import('./core').QwikIntrinsicElements['img'], 'src' | 'width' | 'height' | 'srcSet'>>
+  export default Cmp;
+  export const width: number;
+  export const height: number;
+  export const srcSet: string;
+}
+
+// Image &jsx
+declare module '*&jsx' {
+  const Cmp: import('./core').FunctionComponent<Omit<import('./core').QwikIntrinsicElements['img'], 'src' | 'width' | 'height' | 'srcSet'>>
+  export default Cmp;
+  export const width: number;
+  export const height: number;
+  export const srcSet: string;
+}
