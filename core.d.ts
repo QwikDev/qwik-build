2f1aedbbb86352d06ad01a2329abc97d702b0422
Wout Mertens <Wout.Mertens@gmail.com>
2024-01-27 17:55:08 +0000

    a0d97e3 chore: lint++
    https://github.com/BuilderIO/qwik/commit/a0d97e3887ba002fae24841663fe7ad4bc15b4d6

--- core.d.ts
+++ core.d.ts
@@ -3765,7 +3765,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 1.4.1
+ * 1.4.2
  *
  * @public
  */
--- core.mjs
+++ core.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 1.4.1
+ * @builder.io/qwik 1.4.2
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
@@ -1194,7 +1194,7 @@ const wrap = (value, containerState) => {
  *
  * @public
  */
-const version = "1.4.1";
+const version = "1.4.2";
 
 const hashCode = (text, hash = 0) => {
     for (let i = 0; i < text.length; i++) {
