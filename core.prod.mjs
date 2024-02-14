15e88db860c44e3afcb608cb91bf17fb97c0bd64
Alexander Shushunov <AShushunov@mail.ru>
2024-02-14 07:33:54 +0000

    58b01ef fix(create-qwik): update engine version in package.json for generated projects (#5849)
    https://github.com/BuilderIO/qwik/commit/58b01ef26cf2f02d566d5154b50b433db77a313e

--- core.prod.mjs
+++ core.prod.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 1.4.4
+ * @builder.io/qwik 1.4.5
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
@@ -823,7 +823,7 @@ const static_subtree = 2;
 
 const dangerouslySetInnerHTML = "dangerouslySetInnerHTML";
 
-const version = "1.4.4";
+const version = "1.4.5";
 
 const hashCode = (text, hash = 0) => {
     for (let i = 0; i < text.length; i++) {
@@ -902,7 +902,7 @@ const _renderSSR = async (node, opts) => {
     const containerAttributes = {
         ...opts.containerAttributes,
         "q:container": "paused",
-        "q:version": "1.4.4",
+        "q:version": "1.4.5",
         "q:render": qRender,
         "q:base": opts.base,
         "q:locale": opts.serverData?.locale,
@@ -5499,7 +5499,7 @@ const renderRoot = async (rCtx, parent, jsxOutput) => {
 const getElement = docOrElm => isDocument(docOrElm) ? docOrElm.documentElement : docOrElm;
 
 const injectQContainer = containerEl => {
-    directSetAttribute(containerEl, "q:version", "1.4.4"), directSetAttribute(containerEl, "q:container", "resumed"), 
+    directSetAttribute(containerEl, "q:version", "1.4.5"), directSetAttribute(containerEl, "q:container", "resumed"), 
     directSetAttribute(containerEl, "q:render", "dom");
 };
 
--- optimizer.mjs
+++ optimizer.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/optimizer 1.4.4
+ * @builder.io/qwik/optimizer 1.4.5
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
@@ -1233,7 +1233,7 @@ var QWIK_BINDING_MAP = {
 };
 
 var versions = {
-  qwik: "1.4.4"
+  qwik: "1.4.5"
 };
 
 async function getSystem() {
