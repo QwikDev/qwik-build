68303066a9e1e25c2ed963c71ce8667a140c481a
Jordan White <code.u6ac@jordanwhite.org>
2024-03-17 09:19:19 +0000

    da0a2c2 add magic attr to select Link
    https://github.com/BuilderIO/qwik/commit/da0a2c2504256f26c86aa1d1fb2869268d2a9c0f

--- cli.cjs
+++ cli.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/cli 1.5.1-dev20240313174652
+ * @builder.io/qwik/cli 1.5.1-dev20240317090214
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/BuilderIO/qwik/blob/main/LICENSE
@@ -4986,7 +4986,7 @@ async function printHelp(app) {
   await runCommand2(Object.assign(app, { task: args[0], args }));
 }
 function printVersion() {
-  console.log("1.5.1-dev20240313174652");
+  console.log("1.5.1-dev20240317090214");
 }
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
