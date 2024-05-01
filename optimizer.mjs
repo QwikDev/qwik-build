2598f59322457f6dbf5ca096eae90ffb4326fc4d
Nick K <nick.kruchinin@gmail.com>
2024-05-01 00:23:32 +0000

    f62c103 chore(starters): Add a note about what qwik.env.d.ts file does
    https://github.com/QwikDev/qwik/commit/f62c1039fa32bb3db8cd77f8d9a41efc3ecf890b

--- optimizer.mjs
+++ optimizer.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/optimizer 1.5.2-dev20240501000720
+ * @builder.io/qwik/optimizer 1.5.2-dev20240501000913
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -1282,7 +1282,7 @@ var QWIK_BINDING_MAP = {
 };
 
 var versions = {
-  qwik: "1.5.2-dev20240501000720"
+  qwik: "1.5.2-dev20240501000913"
 };
 
 async function getSystem() {
--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/server 1.5.2-dev20240501000720
+ * @builder.io/qwik/server 1.5.2-dev20240501000913
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -111,7 +111,7 @@ function getBuildBase(opts) {
   return "/build/";
 }
 var versions = {
-  qwik: "1.5.2-dev20240501000720",
+  qwik: "1.5.2-dev20240501000913",
   qwikDom: "2.1.19"
 };
 
