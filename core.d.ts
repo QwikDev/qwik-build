52e4244f69eca5e6eab3c0421a303f48a17d42db
GitStart <1501599+gitstart@users.noreply.github.com>
2024-05-10 23:04:02 +0000

    b8bdc31 feat: When closing over values in `server$` the values should be marked as readonly (#5238)
    https://github.com/QwikDev/qwik/commit/b8bdc31bee6497e279a132512c6f8496430a3267

--- core.d.ts
+++ core.d.ts
@@ -3777,7 +3777,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 1.5.4-dev20240510212023
+ * 1.5.4-dev20240510224927
  *
  * @public
  */
--- server.cjs
+++ server.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/server 1.5.4-dev20240510212023
+ * @builder.io/qwik/server 1.5.4-dev20240510224927
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -155,7 +155,7 @@ function getBuildBase(opts) {
   return "/build/";
 }
 var versions = {
-  qwik: "1.5.4-dev20240510212023",
+  qwik: "1.5.4-dev20240510224927",
   qwikDom: "2.1.19"
 };
 
