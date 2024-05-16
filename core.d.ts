fb2f534cbee5c91c150049a75e5dfc6577af0b2f
PatrickJS <github@patrickjs.com>
2024-05-16 05:43:58 +0000

    d79b934 feat(RouteNavigate): nav(-1) goBack history support (#6324)
    https://github.com/QwikDev/qwik/commit/d79b934a155ac6a0cf7f9ead154825710e324115

--- core.d.ts
+++ core.d.ts
@@ -3779,7 +3779,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 1.5.4-dev20240515210402
+ * 1.5.4-dev20240516053013
  *
  * @public
  */
--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/server 1.5.4-dev20240515210402
+ * @builder.io/qwik/server 1.5.4-dev20240516053013
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -111,7 +111,7 @@ function getBuildBase(opts) {
   return `${import.meta.env.BASE_URL}build/`;
 }
 var versions = {
-  qwik: "1.5.4-dev20240515210402",
+  qwik: "1.5.4-dev20240516053013",
   qwikDom: "2.1.19"
 };
 
