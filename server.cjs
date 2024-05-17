47c95def3716f58b429cfa6bf17dd810e48dcf82
PatrickJS <github@patrickjs.com>
2024-05-17 01:58:17 +0000

    a2f3739 chore(TextEncoderStream): better polyfill tests (#6310)
    https://github.com/QwikDev/qwik/commit/a2f37398057318dd6f99f8a9e763a876cc0c89a4

--- server.cjs
+++ server.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/server 1.5.4-dev20240516053013
+ * @builder.io/qwik/server 1.5.4-dev20240517014251
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -156,7 +156,7 @@ function getBuildBase(opts) {
   return `${import_meta.env.BASE_URL}build/`;
 }
 var versions = {
-  qwik: "1.5.4-dev20240516053013",
+  qwik: "1.5.4-dev20240517014251",
   qwikDom: "2.1.19"
 };
 
