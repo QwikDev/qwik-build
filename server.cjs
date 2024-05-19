2829eca2a7255092e5c90e7006fe6d3a173632d9
Nemanja Granic <90414340+gnemanja@users.noreply.github.com>
2024-05-19 18:03:29 +0000

    7708f71 fix(qwik-city): Removed limiter from recursive path matching (#6327)
    https://github.com/QwikDev/qwik/commit/7708f712e1653723ee6fd7326db0aecf84cbc5a6

--- server.cjs
+++ server.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/server 1.5.4-dev20240518003805
+ * @builder.io/qwik/server 1.5.4-dev20240519030405
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -156,7 +156,7 @@ function getBuildBase(opts) {
   return `${import_meta.env.BASE_URL}build/`;
 }
 var versions = {
-  qwik: "1.5.4-dev20240518003805",
+  qwik: "1.5.4-dev20240519030405",
   qwikDom: "2.1.19"
 };
 
