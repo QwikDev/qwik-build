a6d76b329b0f1b23261506a8f18f0125b845b146
Okiki Ojo <hey@okikio.dev>
2024-05-20 07:33:46 +0000

    a6a8525 fix(qwik-city): 404 resources for less bandwidth (#6341)
    https://github.com/QwikDev/qwik/commit/a6a8525cd676c6e9c8e92ddf00466635d6c78c47

--- core.d.ts
+++ core.d.ts
@@ -3779,7 +3779,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 1.5.4-dev20240519030405
+ * 1.5.4-dev20240520071843
  *
  * @public
  */
--- server.cjs
+++ server.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/server 1.5.4-dev20240519030405
+ * @builder.io/qwik/server 1.5.4-dev20240520071843
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -156,7 +156,7 @@ function getBuildBase(opts) {
   return `${import_meta.env.BASE_URL}build/`;
 }
 var versions = {
-  qwik: "1.5.4-dev20240519030405",
+  qwik: "1.5.4-dev20240520071843",
   qwikDom: "2.1.19"
 };
 
