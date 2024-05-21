1d694eb784872604cfe43e4d66caea1b086002e6
PatrickJS <github@patrickjs.com>
2024-05-21 01:55:16 +0000

    f38f153 fix(server$): return content type changes (#6345)
    https://github.com/QwikDev/qwik/commit/f38f1539edac0b0a4ad31b61003f00f548d2a523

--- core.prod.mjs
+++ core.prod.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 1.5.4-dev20240520071843
+ * @builder.io/qwik 1.5.4-dev20240521013942
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -823,7 +823,7 @@ const static_subtree = 2;
 
 const dangerouslySetInnerHTML = "dangerouslySetInnerHTML";
 
-const version = "1.5.4-dev20240520071843";
+const version = "1.5.4-dev20240521013942";
 
 const hashCode = (text, hash = 0) => {
     for (let i = 0; i < text.length; i++) {
