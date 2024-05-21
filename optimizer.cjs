8ed564068dcb9e24d81aa96ae934d9f79392891e
Maksim Huretski <6115003@gmail.com>
2024-05-21 04:03:20 +0000

    86dcf1d feat(qwik-city): .append() cookie headers to response (#6342)
    https://github.com/QwikDev/qwik/commit/86dcf1d0e2254df5ea41cc4fefa10a3f87fa10ef

--- optimizer.cjs
+++ optimizer.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/optimizer 1.5.4-dev20240521013942
+ * @builder.io/qwik/optimizer 1.5.4-dev20240521034851
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -1256,7 +1256,7 @@ globalThis.qwikOptimizer = function(module) {
     }
   };
   var versions = {
-    qwik: "1.5.4-dev20240521013942"
+    qwik: "1.5.4-dev20240521034851"
   };
   async function getSystem() {
     const sysEnv = getEnv();
