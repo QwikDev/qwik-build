e81fd230b41f800ed99ff21aca236ad81c3fc5f0
PatrickJS <github@patrickjs.com>
2024-05-22 09:51:48 +0000

    cae4e5b fix: all charSet utf-8 issues (#6357)
    https://github.com/QwikDev/qwik/commit/cae4e5bc3b77637264651ec6537b8c72fb75bb28

--- core.d.ts
+++ core.d.ts
@@ -2512,7 +2512,7 @@ declare type SpecialAttrs = {
         children?: undefined;
     };
     meta: {
-        charSet?: string | undefined;
+        charset?: 'UTF-8' | undefined;
         children?: undefined;
     };
     meter: {
@@ -3779,7 +3779,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 1.5.5-dev20240522073908
+ * 1.5.5-dev20240522093807
  *
  * @public
  */
