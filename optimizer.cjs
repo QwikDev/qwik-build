37f9ca52653dfcd8e15a09f998d75d9c0663018e
Giorgio Boa <35845425+gioboa@users.noreply.github.com>
2024-06-14 06:45:46 +0000

    f3cb2d5 revert: expose MDX transformer and context (#6543)
    https://github.com/QwikDev/qwik/commit/f3cb2d52676c39b5b89538bb081343ba692467e8

--- optimizer.cjs
+++ optimizer.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/optimizer 1.5.7-dev20240614052229
+ * @builder.io/qwik/optimizer 1.5.7-dev20240614063244
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -1256,7 +1256,7 @@ globalThis.qwikOptimizer = function(module) {
     }
   };
   var versions = {
-    qwik: "1.5.7-dev20240614052229"
+    qwik: "1.5.7-dev20240614063244"
   };
   async function getSystem() {
     const sysEnv = getEnv();
--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/server 1.5.7-dev20240614052229
+ * @builder.io/qwik/server 1.5.7-dev20240614063244
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -111,7 +111,7 @@ function getBuildBase(opts) {
   return `${import.meta.env.BASE_URL}build/`;
 }
 var versions = {
-  qwik: "1.5.7-dev20240614052229",
+  qwik: "1.5.7-dev20240614063244",
   qwikDom: "2.1.19"
 };
 
