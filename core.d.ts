b9a7db70e0355b7e51cb685fc021327bbe317165
StEve YoUng <2747745470@qq.com>
2024-07-02 03:37:44 +0000

    8a5ef13 docs(starts): fix typo depencies (#6631)
    https://github.com/QwikDev/qwik/commit/8a5ef13c27a41a3521cfdaef87d9d39b7ddb2d86

--- core.d.ts
+++ core.d.ts
@@ -3836,7 +3836,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 1.6.0-dev20240630230742
+ * 1.6.0-dev20240702032412
  *
  * @public
  */
--- core.prod.mjs
+++ core.prod.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 1.6.0-dev20240630230742
+ * @builder.io/qwik 1.6.0-dev20240702032412
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -592,7 +592,7 @@ const serializeSStyle = scopeIds => {
     }
 };
 
-const version = "1.6.0-dev20240630230742";
+const version = "1.6.0-dev20240702032412";
 
 const useSequentialScope = () => {
     const iCtx = useInvokeContext();
