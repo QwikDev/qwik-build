e96d4c6c781a6ac8845b75874f4b7f602659956b
Genki Takiuchi <genki@s21g.com>
2024-07-09 21:58:33 +0000

    0588230 fix(eslint): Fixed lint problem about the `JSXNode` (#6637)
    https://github.com/QwikDev/qwik/commit/05882303c0ee5b0efe784a428436be0440d8ecf7

--- optimizer.mjs
+++ optimizer.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/optimizer 1.6.0-dev20240709163403
+ * @builder.io/qwik/optimizer 1.7.0-dev20240709214526
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -1282,7 +1282,7 @@ var QWIK_BINDING_MAP = {
 };
 
 var versions = {
-  qwik: "1.6.0-dev20240709163403"
+  qwik: "1.7.0-dev20240709214526"
 };
 
 async function getSystem() {
--- server.cjs
+++ server.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/server 1.6.0-dev20240709163403
+ * @builder.io/qwik/server 1.7.0-dev20240709214526
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -159,7 +159,7 @@ function getBuildBase(opts) {
   return `${import_meta.env.BASE_URL}build/`;
 }
 var versions = {
-  qwik: "1.6.0-dev20240709163403",
+  qwik: "1.7.0-dev20240709214526",
   qwikDom: "2.1.19"
 };
 
