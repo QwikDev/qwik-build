0d8efd1dfa24789f3431193afadcc662e64cf7ab
Shai Reznik <shairez@users.noreply.github.com>
2024-07-17 22:38:44 +0000

    8125f6b chore: removed changeset test package
    https://github.com/QwikDev/qwik/commit/8125f6b3a8817d16f3593efda24ebb4b4263afd4

--- server.cjs
+++ server.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/server 0.0.0-dev20240717190632
+ * @builder.io/qwik/server 0.0.0-dev20240717222552
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -159,7 +159,7 @@ function getBuildBase(opts) {
   return `${import_meta.env.BASE_URL}build/`;
 }
 var versions = {
-  qwik: "0.0.0-dev20240717190632",
+  qwik: "0.0.0-dev20240717222552",
   qwikDom: "2.1.19"
 };
 
--- testing/index.cjs
+++ testing/index.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/testing 0.0.0-dev20240717190632
+ * @builder.io/qwik/testing 0.0.0-dev20240717222552
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
