11fc5504b248dfd39f7b4ce130127486602f5307
Wout Mertens <Wout.Mertens@gmail.com>
2025-01-30 14:27:48 +0000

    da69196 fix(core dev): copy optimizer from CI build if dev (#7283)
    https://github.com/QwikDev/qwik/commit/da69196d773c6c007123dde66d538de70ae0c292

--- dist/core.prod.cjs
+++ dist/core.prod.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 1.12.0-dev+d016bf3
+ * @builder.io/qwik 1.12.0-dev+da69196
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -352,7 +352,7 @@
             return value;
         }
     };
-    const version = "1.12.0-dev+d016bf3";
+    const version = "1.12.0-dev+da69196";
     const useSequentialScope = () => {
         const iCtx = useInvokeContext();
         const elCtx = getContext(iCtx.$hostElement$, iCtx.$renderCtx$.$static$.$containerState$);
--- dist/server.mjs
+++ dist/server.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/server 1.12.0-dev+d016bf3
+ * @builder.io/qwik/server 1.12.0-dev+da69196
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -315,7 +315,7 @@ function getBuildBase(opts) {
   return `${import.meta.env.BASE_URL}build/`;
 }
 var versions2 = {
-  qwik: "1.12.0-dev+d016bf3",
+  qwik: "1.12.0-dev+da69196",
   qwikDom: "2.1.19"
 };
 
