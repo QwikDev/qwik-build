603569eff2590ec74ea34e80a24abf8b5aca9452
Wout Mertens <Wout.Mertens@gmail.com>
2025-02-04 08:37:36 +0000

    c98f44c Merge pull request #7308 from QwikDev/fix-ci-pnpm
    https://github.com/QwikDev/qwik/commit/c98f44c409a72a6fb9464430b44c1de4a864d088

--- dist/core.mjs
+++ dist/core.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 1.12.0-dev+0a254ea
+ * @builder.io/qwik 1.12.0-dev+c98f44c
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -919,7 +919,7 @@ const serializeSStyle = (scopeIds) => {
  *
  * @public
  */
-const version = "1.12.0-dev+0a254ea";
+const version = "1.12.0-dev+c98f44c";
 
 /**
  * @internal
--- dist/optimizer.cjs
+++ dist/optimizer.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/optimizer 1.12.0-dev+0a254ea
+ * @builder.io/qwik/optimizer 1.12.0-dev+c98f44c
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -1226,7 +1226,7 @@ globalThis.qwikOptimizer = function(module) {
   }
   var QWIK_BINDING_MAP = {};
   var versions = {
-    qwik: "1.12.0-dev+0a254ea"
+    qwik: "1.12.0-dev+c98f44c"
   };
   async function getSystem() {
     const sysEnv = getEnv();
