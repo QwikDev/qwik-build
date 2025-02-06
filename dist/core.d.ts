d654a8a3f367f340a24933e5b0d819c8e201c33f
Wout Mertens <Wout.Mertens@gmail.com>
2025-02-06 12:24:31 +0000

    970d41f Merge pull request #7317 from QwikDev/fix-insights
    https://github.com/QwikDev/qwik/commit/970d41f4383fbf3428a01952f889ebeb3b7172c2

--- dist/core.d.ts
+++ dist/core.d.ts
@@ -3875,7 +3875,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 1.12.0-dev+c98f44c
+ * 1.12.0-dev+970d41f
  *
  * @public
  */
--- dist/core.prod.mjs
+++ dist/core.prod.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 1.12.0-dev+c98f44c
+ * @builder.io/qwik 1.12.0-dev+970d41f
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -579,7 +579,7 @@ const serializeSStyle = scopeIds => {
     }
 };
 
-const version = "1.12.0-dev+c98f44c";
+const version = "1.12.0-dev+970d41f";
 
 const useSequentialScope = () => {
     const iCtx = useInvokeContext();
