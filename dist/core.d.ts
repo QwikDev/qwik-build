2c4dc390e73cf3723ea1184784605a50520ac8f9
Sreekanth Nagareddy <sreeisalso@gmail.com>
2025-02-21 13:37:53 +0000

    f57e049 docs: update rehype and make accessibility CSS changes (#7358)
    https://github.com/QwikDev/qwik/commit/f57e049599266c64ed75bd391af924d14dc9da02

--- dist/core.d.ts
+++ dist/core.d.ts
@@ -3875,7 +3875,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 1.12.0-dev+d8bf779
+ * 1.12.0-dev+f57e049
  *
  * @public
  */
