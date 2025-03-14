d9354a448cf3e2101e53a128b2d40197e8f7fb9a
Shane-Donlon <130906067+Shane-Donlon@users.noreply.github.com>
2025-03-14 21:28:05 +0000

    62e5f9a chore(scripts): improve windows support (#7399)
    https://github.com/QwikDev/qwik/commit/62e5f9a052c3dfbeab7c2d407ff0dfcac36585f4

--- dist/core.prod.cjs
+++ dist/core.prod.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 1.12.1-dev+0cf1eaf
+ * @builder.io/qwik 1.12.1-dev+62e5f9a
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -352,7 +352,7 @@
             return value;
         }
     };
-    const version = "1.12.1-dev+0cf1eaf";
+    const version = "1.12.1-dev+62e5f9a";
     const useSequentialScope = () => {
         const iCtx = useInvokeContext();
         const elCtx = getContext(iCtx.$hostElement$, iCtx.$renderCtx$.$static$.$containerState$);
--- dist/server.mjs
+++ dist/server.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/server 1.12.1-dev+0cf1eaf
+ * @builder.io/qwik/server 1.12.1-dev+62e5f9a
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -315,7 +315,7 @@ function getBuildBase(opts) {
   return `${import.meta.env.BASE_URL}build/`;
 }
 var versions2 = {
-  qwik: "1.12.1-dev+0cf1eaf",
+  qwik: "1.12.1-dev+62e5f9a",
   qwikDom: "2.1.19"
 };
 
