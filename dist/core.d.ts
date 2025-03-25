4ef427a1cc7bb7886919284d92bd2613674b2bf1
Shai Reznik <shairez@users.noreply.github.com>
2025-03-25 22:25:04 +0000

    e686f63 update changesets versions (#7457)
    https://github.com/QwikDev/qwik/commit/e686f63e3e5ee1db2455969cc3e8f62c3f0f8277

--- dist/core.d.ts
+++ dist/core.d.ts
@@ -3879,7 +3879,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 1.12.1-dev+66d8c7f
+ * 1.12.1-dev+e686f63
  *
  * @public
  */
