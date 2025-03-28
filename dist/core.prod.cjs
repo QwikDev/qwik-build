47d631e6a6781ef16bd0984995920ce78ca3a59a
Wout Mertens <Wout.Mertens@gmail.com>
2025-03-28 16:07:06 +0000

    e089e16 Merge pull request #6966 from Kampouse/pr-website-search-button
    https://github.com/QwikDev/qwik/commit/e089e164be1fa0a680f929aad633dc2a2315a4c1

--- dist/core.prod.cjs
+++ dist/core.prod.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 1.12.1-dev+e686f63
+ * @builder.io/qwik 1.12.1-dev+e089e16
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -352,7 +352,7 @@
             return value;
         }
     };
-    const version = "1.12.1-dev+e686f63";
+    const version = "1.12.1-dev+e089e16";
     const useSequentialScope = () => {
         const iCtx = useInvokeContext();
         const elCtx = getContext(iCtx.$hostElement$, iCtx.$renderCtx$.$static$.$containerState$);
