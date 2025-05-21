99d58682d17973fb8302c245c01b463024cbd568
Shai Reznik <shairez@users.noreply.github.com>
2025-05-21 13:45:13 +0000

    19ad014 Merge pull request #7486 from QwikDev/changeset-release/upcoming
    https://github.com/QwikDev/qwik/commit/19ad014d35d2eb42dcee7e574406bc23b62dbfc7

--- dist/core.cjs
+++ dist/core.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 1.13.0-dev+2ff5773
+ * @builder.io/qwik 1.14.0-dev+19ad014
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -922,7 +922,7 @@
      *
      * @public
      */
-    const version = "1.13.0-dev+2ff5773";
+    const version = "1.14.0-dev+19ad014";
 
     /**
      * @internal
