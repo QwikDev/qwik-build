4f0745cd447997dc7d93478ffbe62bc52e88b494
Wout Mertens <Wout.Mertens@gmail.com>
2025-07-21 17:43:22 +0000

    1cd73f1 Merge pull request #7750 from QwikDev/upcoming
    https://github.com/QwikDev/qwik/commit/1cd73f1b16f074fbe1f7f783e2d9192343eb8ae0

--- dist/core.prod.mjs
+++ dist/core.prod.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 1.14.1-dev+7843e78
+ * @builder.io/qwik 1.15.0-dev+1cd73f1
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -581,7 +581,7 @@ const serializeSStyle = scopeIds => {
     }
 };
 
-const version = "1.14.1-dev+7843e78";
+const version = "1.15.0-dev+1cd73f1";
 
 const useSequentialScope = () => {
     const iCtx = useInvokeContext();
--- dist/optimizer.mjs
+++ dist/optimizer.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/optimizer 1.14.1-dev+7843e78
+ * @builder.io/qwik/optimizer 1.15.0-dev+1cd73f1
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -1260,7 +1260,7 @@ function createPath(opts = {}) {
 var QWIK_BINDING_MAP = {};
 
 var versions = {
-  qwik: "1.14.1-dev+7843e78"
+  qwik: "1.15.0-dev+1cd73f1"
 };
 
 async function getSystem() {
