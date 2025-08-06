737df4e525944e8ae686f4f0f4f7d6e41143dab0
Wout Mertens <Wout.Mertens@gmail.com>
2025-08-06 08:58:12 +0000

    9bcb4ee Merge pull request #7783 from gioboa/feat/bump-prettier
    https://github.com/QwikDev/qwik/commit/9bcb4eefacb1106db1563b669ad842bec1d51177

--- dist/core.mjs
+++ dist/core.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 1.15.0-dev+47035a9
+ * @builder.io/qwik 1.15.0-dev+9bcb4ee
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -920,7 +920,7 @@ const serializeSStyle = (scopeIds) => {
  *
  * @public
  */
-const version = "1.15.0-dev+47035a9";
+const version = "1.15.0-dev+9bcb4ee";
 
 /**
  * @internal
--- dist/server.cjs
+++ dist/server.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/server 1.15.0-dev+47035a9
+ * @builder.io/qwik/server 1.15.0-dev+9bcb4ee
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -704,7 +704,7 @@ function getBuildBase(opts) {
   return `${"globalThis.BASE_URL||'/'"}build/`;
 }
 var versions = {
-  qwik: "1.15.0-dev+47035a9",
+  qwik: "1.15.0-dev+9bcb4ee",
   qwikDom: "2.1.19"
 };
 
