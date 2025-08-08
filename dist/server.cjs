c617a59e0e43914641601d579d1cf68c16c1af59
Giorgio Boa <35845425+gioboa@users.noreply.github.com>
2025-08-08 07:38:35 +0000

    30cb4f3 chore: bump Vite to v 7.1.0 🥦 (#7792)
    https://github.com/QwikDev/qwik/commit/30cb4f3ac44c2241b17d530fdd29aad8355a4fc1

--- dist/server.cjs
+++ dist/server.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/server 1.15.0-dev+3c8c727
+ * @builder.io/qwik/server 1.15.0-dev+30cb4f3
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -704,7 +704,7 @@ function getBuildBase(opts) {
   return `${"globalThis.BASE_URL||'/'"}build/`;
 }
 var versions = {
-  qwik: "1.15.0-dev+3c8c727",
+  qwik: "1.15.0-dev+30cb4f3",
   qwikDom: "2.1.19"
 };
 
