c3dc9ec6e7155a7f634dbbb54f76f9041dd1b4c1
Giorgio Boa <35845425+gioboa@users.noreply.github.com>
2025-08-26 04:00:10 +0000

    e02d8d8 Merge pull request #7867 from QwikDev/dependabot/npm_and_yarn/npm_and_yarn-f4b2ca0141
    https://github.com/QwikDev/qwik/commit/e02d8d81549456440861c51b6e77232c97b286ea

--- dist/server.cjs
+++ dist/server.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/server 1.15.0-dev+8229a89
+ * @builder.io/qwik/server 1.15.0-dev+e02d8d8
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -705,7 +705,7 @@ function getBuildBase(opts) {
   return `${"globalThis.BASE_URL||'/'"}build/`;
 }
 var versions = {
-  qwik: "1.15.0-dev+8229a89",
+  qwik: "1.15.0-dev+e02d8d8",
   qwikDom: "2.1.19"
 };
 
