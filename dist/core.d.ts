d10ff7a63844e62c83ea7aa9bab9c7fcdb5111a5
Wout Mertens <Wout.Mertens@gmail.com>
2025-09-03 16:34:10 +0000

    4f128c9 Merge pull request #7908 from maiieul/set-packages-as-side-effect-free
    https://github.com/QwikDev/qwik/commit/4f128c9088fea5865b6d0b6e9086502cd54a6949

--- dist/core.d.ts
+++ dist/core.d.ts
@@ -3858,7 +3858,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 1.16.0-dev+834850b
+ * 1.16.0-dev+4f128c9
  *
  * @public
  */
