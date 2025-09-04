675feba773c0f05bd1a39df2653b3a29ff07b73e
Giorgio Boa <35845425+gioboa@users.noreply.github.com>
2025-09-04 10:57:03 +0000

    9fa4d26 chore: change build target 🍻 (#7909)
    https://github.com/QwikDev/qwik/commit/9fa4d265b9a1163321a6292b840f4b6ed4be699c

--- dist/core.d.ts
+++ dist/core.d.ts
@@ -3858,7 +3858,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 1.16.0-dev+4f128c9
+ * 1.16.0-dev+9fa4d26
  *
  * @public
  */
