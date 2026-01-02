58a9c7ca88ebe148b137f6ccb9bfe4293ecc24f2
Kermout Ayoub <93722724+LazyClicks@users.noreply.github.com>
2026-01-02 12:54:55 +0000

    a748c30 feat: cloudflare workers adapter (#8214)
    https://github.com/QwikDev/qwik/commit/a748c30057a3a1147647603d82679deaf4203e65

--- dist/server.cjs
+++ dist/server.cjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik/server 1.17.1-dev+246b4e4
+ * @builder.io/qwik/server 1.17.1-dev+a748c30
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -702,7 +702,7 @@ function getBuildBase(opts) {
   return `${"globalThis.BASE_URL||'/'"}build/`;
 }
 var versions = {
-  qwik: "1.17.1-dev+246b4e4",
+  qwik: "1.17.1-dev+a748c30",
   qwikDom: "2.1.19"
 };
 
