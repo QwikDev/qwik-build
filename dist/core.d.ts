c504f56edf27aff21338a588ee6c49b2804bfd21
Alexis Mch <57993754+alexismch@users.noreply.github.com>
2026-01-10 17:43:17 +0000

    e21f93b feat(QwikCityMockProvider): custom loaders & actions mocks (#8102)
    https://github.com/QwikDev/qwik/commit/e21f93bd8702b07701cb1bd9583396b8cf9959c1

--- dist/core.d.ts
+++ dist/core.d.ts
@@ -3858,7 +3858,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 1.17.1-dev+a748c30
+ * 1.17.1-dev+e21f93b
  *
  * @public
  */
