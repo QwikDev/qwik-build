d3fe1e517b21269cbbae95c319f995a71819d5a1
Wout Mertens <Wout.Mertens@gmail.com>
2026-03-01 14:15:14 +0000

    f71fb94 Merge pull request #8395 from azat-io/feat/eslint-10
    https://github.com/QwikDev/qwik/commit/f71fb94bb4d9ec61cd261b70028aad3c8d728577

--- dist/core.d.ts
+++ dist/core.d.ts
@@ -3865,7 +3865,7 @@ export declare type ValueOrPromise<T> = T | Promise<T>;
 export declare const _verifySerializable: <T>(value: T, preMessage?: string) => T;
 
 /**
- * 1.19.0-dev+7a77854
+ * 1.19.0-dev+f71fb94
  *
  * @public
  */
--- dist/core.mjs
+++ dist/core.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 1.19.0-dev+7a77854
+ * @builder.io/qwik 1.19.0-dev+f71fb94
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -922,7 +922,7 @@ const serializeSStyle = (scopeIds) => {
  *
  * @public
  */
-const version = "1.19.0-dev+7a77854";
+const version = "1.19.0-dev+f71fb94";
 
 /**
  * @internal
