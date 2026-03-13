83a356d7b066e4ba4aad3003b8e58ca67b103caf
Wout Mertens <Wout.Mertens@gmail.com>
2026-03-13 13:51:50 +0000

    5994de2 Merge pull request #8426 from QwikDev/changeset-release/upcoming
    https://github.com/QwikDev/qwik/commit/5994de2a025cc978b905a4cd27ffa32af5f35b51

--- dist/core.prod.mjs
+++ dist/core.prod.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 1.19.1
+ * @builder.io/qwik 1.19.2
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -582,7 +582,7 @@ const serializeSStyle = scopeIds => {
     }
 };
 
-const version = "1.19.1";
+const version = "1.19.2";
 
 const useSequentialScope = () => {
     const iCtx = useInvokeContext();
@@ -894,7 +894,7 @@ const _renderSSR = async (node, opts) => {
     const locale = opts.serverData?.locale;
     const containerAttributes = opts.containerAttributes;
     const qRender = containerAttributes["q:render"];
-    containerAttributes["q:container"] = "paused", containerAttributes["q:version"] = "1.19.1", 
+    containerAttributes["q:container"] = "paused", containerAttributes["q:version"] = "1.19.2", 
     containerAttributes["q:render"] = (qRender ? qRender + "-" : "") + "ssr", containerAttributes["q:base"] = opts.base || "", 
     containerAttributes["q:locale"] = locale, containerAttributes["q:manifest-hash"] = opts.manifestHash, 
     containerAttributes["q:instance"] = hash();
@@ -5663,7 +5663,7 @@ const renderRoot = async (rCtx, parent, jsxOutput) => {
 const getElement = docOrElm => isDocument(docOrElm) ? docOrElm.documentElement : docOrElm;
 
 const injectQContainer = containerEl => {
-    directSetAttribute(containerEl, "q:version", "1.19.1"), directSetAttribute(containerEl, "q:container", "resumed"), 
+    directSetAttribute(containerEl, "q:version", "1.19.2"), directSetAttribute(containerEl, "q:container", "resumed"), 
     directSetAttribute(containerEl, "q:render", "dom");
 };
 
