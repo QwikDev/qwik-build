49db25270f5b05175efe0f65a7e2526e21189b6b
Wout Mertens <Wout.Mertens@gmail.com>
2026-03-26 11:47:43 +0000

    dfb162a Merge pull request #8490 from restareaByWeezy/fix/eslint-use-method-render-hook
    https://github.com/QwikDev/qwik/commit/dfb162a940ff6950e79efd9cce0a64cda828cfe4

--- dist/core.prod.mjs
+++ dist/core.prod.mjs
@@ -1,6 +1,6 @@
 /**
  * @license
- * @builder.io/qwik 1.19.0-dev+fdd1ee5
+ * @builder.io/qwik 1.19.0-dev+dfb162a
  * Copyright Builder.io, Inc. All Rights Reserved.
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/QwikDev/qwik/blob/main/LICENSE
@@ -582,7 +582,7 @@ const serializeSStyle = scopeIds => {
     }
 };
 
-const version = "1.19.0-dev+fdd1ee5";
+const version = "1.19.0-dev+dfb162a";
 
 const useSequentialScope = () => {
     const iCtx = useInvokeContext();
